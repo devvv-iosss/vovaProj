3d15cf46712fc189ae2d19ce3d89dcfb7766fe08
ios_dev <futureiosdeveloper@gmail.com>
2021-10-12 07:57:07 +0300

    Vova - commit

---
+++ vovaProj/VovaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VovaViewController.swift
+//  vovaProj
+//
+//  Created by Vladimir Syleimanov on 12.10.2021.
+//
+
+import UIKit
+
+class VovaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
